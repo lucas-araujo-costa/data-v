d28b3df8cf1f5e892d0ee541731a621043265212
Lucas A. <81054783+lucas-araujo-costa@users.noreply.github.com>
2024-07-27 12:18:59 -0300

    fix: fixing TypeAnimation property

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,7 +9,7 @@ const Hero = () => {
         <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data.</h1>
         <div className='flex justify-center items-center'>
           <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Fast, flexible financing for</p>
-          <TypeAnimation sequence={['BTB', 'SASS', 'BTC']} speed={445 as any}  repeat={Infinity} className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'/>
+          <TypeAnimation sequence={['BTB', 'SASS', 'BTC']} speed={Speed}  repeat={Infinity} className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2'/>
         </div>
         <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms</p>
         <button className='bg-[--theme-color] w-[200px] rounded-md font-medium my-6 mx-auto px-6 py-3 text-black'><a href="#cards">Get Started</a></button>
